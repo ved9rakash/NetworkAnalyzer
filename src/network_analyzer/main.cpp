3fa9dd149cbce0c66a7010860de8f81d3cbbb375
ved9rakash <vs19990711@gmail.com>
2024-07-24 14:34:49 +0530

    connect packet capturing to window

--- src/network_analyzer/main.cpp
+++ src/network_analyzer/main.cpp
@@ -1,7 +1,6 @@
 #include <QApplication>
 #include "main_window.h"
 #include "analysis_engine.h"
-#include <iostream>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -10,6 +9,7 @@ int main(int argc, char *argv[]) {
     w.show();
 
     NetworkAnalyzer analyzer("eth0");  // Change to your network interface
+    QObject::connect(&analyzer, &NetworkAnalyzer::packetCaptured, &w, &MainWindow::addPacketInfo);
     analyzer.start();
 
     return app.exec();
