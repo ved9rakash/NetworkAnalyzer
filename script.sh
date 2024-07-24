1232869035a5c452052d4270830fbbee6dd2f7a3
ved9rakash <vs19990711@gmail.com>
2024-07-24 11:10:45 +0530

    script

--- script.sh
+++ script.sh
@@ -3,14 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Define the path to the vcpkg toolchain file
-VCPKG_TOOLCHAIN_FILE="/home/ved9rakash/vcpkg/scripts/buildsystems/vcpkg.cmake"
-
-# Run CMake with the specified preset and toolchain file
-cmake --preset=default -DCMAKE_TOOLCHAIN_FILE="${VCPKG_TOOLCHAIN_FILE}" --log-level="VERBOSE"
-
-# Build the project using make
-make -C ./cmake/build-debug/
-
-# Run the built executable
-./cmake/build-debug/src/network_analyzer
+cmake --preset default --log-level=VERBOSE
+ninja -C cmake/build-debug/ --verbose
+sudo ./cmake/build-debug/src/network_analyzer
