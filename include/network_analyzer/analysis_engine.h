e794d16d6d48a4e9d602ff9e33eef34410a83263
ved9rakash <vs19990711@gmail.com>
2024-07-24 14:33:06 +0530

    added packet capture

--- include/network_analyzer/analysis_engine.h
+++ include/network_analyzer/analysis_engine.h
@@ -2,18 +2,28 @@
 
 #include "packet_capture.h"
 
+#include "QObject"
+#include "QMainWindow"
+#include "pcapplusplus/RawPacket.h"
 #include "pcapplusplus/PcapLiveDevice.h"
 #include "pcapplusplus/PcapLiveDeviceList.h"
 
 #include <string>
 
-class NetworkAnalyzer {
+class NetworkAnalyzer : public QObject {
+    Q_OBJECT
+
 public:
-    NetworkAnalyzer(const std::string& interface);
+    NetworkAnalyzer(const std::string& interface, QObject* parent=nullptr);
     void start();
+
+signals:
+    void packetCaptured(const QString& info);
 
 private:
     std::string interface;
     pcpp::PcapLiveDevice* dev;
     PacketHandler packetHandler;
+
+    static void packetCallback(pcpp::RawPacket* packet, pcpp::PcapLiveDevice* dev, void* cookie);
 };
