218f6d07d1a2bf40cd818b344fe51610ab9915a2
ved9rakash <vs19990711@gmail.com>
2024-07-27 12:39:48 +0530

    main.cpp

--- src/network_analyzer/main.cpp
+++ src/network_analyzer/main.cpp
@@ -1,16 +1,12 @@
-#include <QApplication>
 #include "main_window.h"
-#include "analysis_engine.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    MainWindow w;
-    w.show();
-
-    NetworkAnalyzer analyzer("eth0");  // Change to your network interface
-    QObject::connect(&analyzer, &NetworkAnalyzer::packetCaptured, &w, &MainWindow::addPacketInfo);
-    analyzer.start();
+    MainWindow mainWindow;
+    mainWindow.show();
 
     return app.exec();
 }
