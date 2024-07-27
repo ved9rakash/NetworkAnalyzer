be0a3aed8c6163c76aa7bd6e4d1f7aa8adffc752
ved9rakash <vs19990711@gmail.com>
2024-07-27 12:36:59 +0530

    updated script

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+rm -rf cmake
 cmake --preset default --log-level=VERBOSE
 ninja -C cmake/build-debug/ --verbose
 sudo ./cmake/build-debug/src/network_analyzer
